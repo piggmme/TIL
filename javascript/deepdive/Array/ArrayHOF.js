ae2ec40aad8a29c472e208d28515ba31a17b1ee2
kheeyaa <kheeyaa@icloud.com>
2021-09-22 22:49:10 +0900

    feat: Update ArrayHOF note

--- javascript/deepdive/Array/ArrayHOF.js
+++ javascript/deepdive/Array/ArrayHOF.js
@@ -217,7 +217,7 @@
 
 //  Array.prototype.filter ------------------------------------------------------------------
 {
-  // 자신을 호출한 배열의 모든 요소를 순회하며 인수로 전달받은 콜백 함수를 반복 호출
+  // 자신을 호출한 배열의 모든 요소를 순회하며 인수로 전달받은 콜백 함수를 반복 호출, 원본 배열을 바꾸지 않는다.
   // 1. 콜백 함수의 반환값이 true인 요소로만 구성된 새로운 배열을 반환
   // 2. 콜백 함수의 인수 (1) 배열의 요소값 (2) 인덱스 (3) 호출한 배열 자체(this)
   // 3. filter 메서드의 두 번째 인수로 filter 메서드 콜백 함수 내부에서 this로 사용할 객체를 전달할 수 있다
@@ -278,17 +278,166 @@
 
 //  Array.prototype.reduce ------------------------------------------------------------------
 {
-  // 자신을 호출한 배열의 모든 요소를 순회하며 인수로 전달받은 콜백 함수를 반복 호출
+  // 자신을 호출한 배열의 모든 요소를 순회하며 인수로 전달받은 콜백 함수를 반복 호출, 원본 배열을 바꾸지 않는다.
   // 1. 콜백 함수의 반환값을 다음 순회 시 콜백 함수의 첫 번째 인수로 전달하면서
-  // 콜백 함수를 호출하여 하나의 결과값을 만들어 반환한다.
+  // 콜백 함수를 호출하여 <하나의 결과값>을 만들어 반환한다.
+  // 2. reduce의 인수 (1) 콜백 함수 (2) 초기값 : 생략은 가능하지만 안전하지 않다.
+  // 3. 콜백 함수의 인수 (1) 초기값 또는 함수의 이전 반환값 (2) 배열의 요소값 (3) 인덱스 (4) 호출한 배열 자체(this)
+
+  // 누적 합 구하기
+  {
+    const sum = [1, 2, 3, 4].reduce(
+      (accumulator, currentValue, index, array) => accumulator + currentValue,
+      0
+    );
+    console.log(sum); // 10
+  }
+
+  // 평균 구하기
+  {
+    const values = [1, 2, 3, 4, 5, 6];
+    const average = values.reduce((acc, cur, i, { length }) => {
+      return i === length - 1 ? (acc + cur) / length : acc + cur;
+    }, 0);
+    console.log(average); // 3.5
+  }
+
+  // 최대값 구하기
+  {
+    const values = [1, 2, 3, 4, 5];
+    const max = values.reduce((acc, cur) => (acc > cur ? acc : cur), 0);
+    console.log(max); // 5
+  }
+
+  // 요소의 중복 횟수 구하기
+  {
+    const fruits = ['banana', 'apple', 'orange', 'orange', 'apple'];
+    const count = fruits.reduce((acc, cur) => {
+      acc[cur] = (acc[cur] || 0) + 1;
+      return acc;
+    }, {});
+    // 콜백 함수는 총 5번 호출되고 다음과 같이 결과값을 반환한다.
+    /*
+    {banana: 1} => {banana: 1, apple: 1} => {banana: 1, apple: 1, orange: 1}
+    => {banana: 1, apple: 1, orange: 2} => {banana: 1, apple: 2, orange: 2}
+    */
+    console.log(count); // { banana: 1, apple: 2, orange: 2 }
+  }
+
+  // 중첩 배열 평탄화
+  {
+    const values = [1, [2, 3], 4, [5, 6]];
+    const flatten = values.reduce((acc, cur) => acc.concat(cur), []);
+    console.log(flatten); // [ 1, 2, 3, 4, 5, 6 ]
+  }
+
+  // 중복 요소 제거
+  {
+    const values = [1, 2, 1, 3, 5, 4, 5, 3, 4, 4];
+    const result = values.reduce((acc, cur, i, arr) => {
+      if (arr.indexOf(cur) === i) acc.push(cur);
+      return acc;
+    }, []);
+    console.log(result); // [ 1, 2, 3, 5, 4 ]
+  }
+}
+
+//  Array.prototype.some ------------------------------------------------------------------
+{
+  // 자신을 호출한 배열의 모든 요소를 순회하며 인수로 전달받은 콜백 함수를 반복 호출, 원본 배열을 바꾸지 않는다.
+  // 배열의 요소 중에 콜백 함수를 통해 정의한 조건을 만족하는 요소가 1개 이상 존재하는지 그 결과를 불리언 타입으로 반환한다.
+  // 1. 콜백 함수의 반환값이 단 한 번이라도 참이면 treu, 모두 거짓이면 false, 빈 배열인 경우 언제나 false
   // 2. 콜백 함수의 인수 (1) 배열의 요소값 (2) 인덱스 (3) 호출한 배열 자체(this)
-  // 3. reduce 메서드의 두 번째 인수로 reduce 메서드 콜백 함수 내부에서 this로 사용할 객체를 전달할 수 있다
+  // 3. some 메서드의 두 번째 인수로 some 메서드 콜백 함수 내부에서 this로 사용할 객체를 전달할 수 있다
+
+  [5, 10, 15].some(item => item > 10); // true
+  [5, 10, 15].some(item => item < 0); // false
+  ['apple', 'banana', 'mango'].some(item => item === 'banana'); // true
+  [].some(item => item > 3); // false
 }
 
-//  Array.prototype. ------------------------------------------------------------------
+//  Array.prototype.every ------------------------------------------------------------------
 {
+  // 자신을 호출한 배열의 모든 요소를 순회하며 인수로 전달받은 콜백 함수를 반복 호출, 원본 배열을 바꾸지 않는다.
+  // 배열의 요소 중에 콜백 함수를 통해 정의한 조건을 모두 만족하는지 확인하여 그 결과를 불리언 타입으로 반환한다.
+  // 1. 콜백 함수의 반환값이 모두 참이면 true, 단 한 번이라도 거짓이면 false, 빈 배열인 경우 언제나 true
+  // 2. 콜백 함수의 인수 (1) 배열의 요소값 (2) 인덱스 (3) 호출한 배열 자체(this)
+  // 3. every 메서드의 두 번째 인수로 every 메서드 콜백 함수 내부에서 this로 사용할 객체를 전달할 수 있다
+
+  [5, 10, 15].some(item => item > 3); // true
+  [5, 10, 15].some(item => item > 10); // false
+  [].some(item => item > 3); // true
 }
 
-//  Array.prototype. ------------------------------------------------------------------
+//  Array.prototype.find ------------------------------------------------------------------
 {
+  // 자신을 호출한 배열의 모든 요소를 순회하며 인수로 전달받은 콜백 함수를 반복 호출, 원본 배열을 바꾸지 않는다.
+  // 1. 콜백 함수의 반환값이 true인 첫 번째 요소를 반환한다. 콜백 함수의 반환값이 true인 요소가 없다면 undefined를 반환한다.
+  // 2. 콜백 함수의 인수 (1) 배열의 요소값 (2) 인덱스 (3) 호출한 배열 자체(this)
+  // 3. find 메서드의 두 번째 인수로 find 메서드 콜백 함수 내부에서 this로 사용할 객체를 전달할 수 있다
+  const users = [
+    { id: 1, name: 'Lee' },
+    { id: 2, name: 'Kim' },
+    { id: 2, name: 'Choi' },
+    { id: 3, name: 'Park' },
+  ];
+  // id가 2인 첫 번째 요소를 반환한다. find 메서드는 배열이 아니라 요소를 반환한다.
+  users.find(user => user.id === 2); //  {id: 2, name: 'Kim'}
+
+  // filter 메서드는 배열을 반환한다.
+  [1, 2, 2, 3].filter(item => item === 2); //  [2, 2]
+  // find 메서드는 요소를 반환한다.
+  [1, 2, 2, 3].find(item => item === 2); //  2
+}
+
+//  Array.prototype.findIndex ------------------------------------------------------------------
+{
+  // 자신을 호출한 배열의 모든 요소를 순회하며 인수로 전달받은 콜백 함수를 반복 호출, 원본 배열을 바꾸지 않는다.
+  // 1. 콜백 함수의 반환값이 true인 첫 번째 요소의 인덱스를 반환한다. 콜백 함수의 반환값이 true인 요소가 없다면 -1를 반환한다.
+  // 2. 콜백 함수의 인수 (1) 배열의 요소값 (2) 인덱스 (3) 호출한 배열 자체(this)
+  // 3. findIndex 메서드의 두 번째 인수로 findIndex 메서드 콜백 함수 내부에서 this로 사용할 객체를 전달할 수 있다
+
+  const users = [
+    { id: 1, name: 'Lee' },
+    { id: 2, name: 'Kim' },
+    { id: 2, name: 'Choi' },
+    { id: 3, name: 'Park' },
+  ];
+  // id가 2인 요소의 인덱스를 구한다.
+  users.findIndex(user => user.id === 2); // 1
+  // name이 'Park'인 요소의 인덱스를 구한다.
+  users.findIndex(user => user.name === 'Park'); // 3
+  // 위와 같이 프로퍼티 키와 프로퍼티 값으로 요소의 인덱스를 구하는 경우 다음과 같이 콜백 함수를 추상화할 수 있다.
+  function predicate(key, value) {
+    // key와 value를 기억하는 클로저를 반환
+    return item => item[key] === value;
+  }
+  // id가 2인 요소의 인덱스를 구한다.
+  users.findIndex(predicate('id', 2)); // 1
+
+  // name이 'Park'인 요소의 인덱스를 구한다.
+  users.findIndex(predicate('name', 'Park')); // 3
+}
+
+//  Array.prototype.flatMap ------------------------------------------------------------------
+{
+  // flatMap 메서드는 map 메서드를 통해 생성된 새로운 배열을 평탄화한다.
+  // 즉 map 메서드와 flat 메서드를 순차적으로 실행하는 효과가 있다.
+  const arr = ['hello', 'world'];
+
+  // map과 flat을 순차적으로 실행
+  arr.map(x => x.split('')).flat(); // ['h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd']
+
+  // flatMap은 map을 통해 생성된 새로운 배열을 평탄화한다.
+  arr.flatMap(x => x.split('')); // ['h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd']
+
+  // 단, flatMap 메서드는 flat 메서드처럼 인수를 전달하여 평탄화 깊이를 지정할 수는 없고 1단계만 평탄화한다.
+  // map 메서드를 통해 생성된 중첩 배열의 평탄화 깊이를 지정해야 하면 flatMap 메서드를 사용하지 말고 map 메서드와 flat 메서드를 각각 호출한다.
+  const arr = ['hello', 'world'];
+
+  // flatMap은 1단계만 평탄화한다.
+  arr.flatMap((str, index) => [index, [str, str.length]]); // [[0, ['hello', 5]], [1, ['world', 5]]] => [0, ['hello', 5], 1, ['world', 5]]
+
+  // 평탄화 깊이를 지정해야 하면 flatMap 메서드를 사용하지 말고 map 메서드와 flat 메서드를 각각 호출한다.
+  arr.map((str, index) => [index, [str, str.length]]).flat(2); // [[0, ['hello', 5]], [1, ['world', 5]]] => [0, 'hello', 5, 1, 'world', 5]
 }
