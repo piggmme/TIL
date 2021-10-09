1fa996260607fabc8961d84c7eadeb1579d38e48
kheeyaa <kheeyaa@icloud.com>
2021-10-09 19:09:41 +0900

    feat: Add algorithm about RegExp

--- playground/dev/3)String/2.js
+++ playground/dev/3)String/2.js
@@ -1,7 +1,23 @@
 // 신규 아이디 추천
 // https://programmers.co.kr/learn/courses/30/lessons/72410?language=javascript
-
-const makeNewId = newId =>
-  newId.replace(/[A-Z]/g, match => match.toLowerCase().replace(/[a-z0-9]/g, ''));
-
-console.log(makeNewId('...!@BaT#*..y.abcdefghijklm'));
+const solution = newId =>
+  newId
+    .replace(/[A-Z]/g, match => match.toLowerCase())
+    .replace(/[^a-z0-9-_.]/g, '')
+    .replace(/[.]+/g, '.')
+    .replace(/(^[.])|([.]$)/g, '')
+    .replace(/^$/g, 'a')
+    .replace(/.+/g, match => (match.length >= 16 ? match.substring(0, 15) : match))
+    .replace(/[.]$/g, '')
+    .replace(/.{0,}/g, match =>
+      match.length === 2
+        ? match[0] + match[1].repeat(2)
+        : match.length === 1
+        ? match.repeat(3)
+        : match
+    );
+console.log(solution('=.=')); // aaa
+console.log(solution('...!@BaT#*..y.abcdefghijklm')); // bat.y.abcdefghi
+console.log(solution('z-+.^.')); // z--
+console.log(solution('123_.def')); // 123_.def
+console.log(solution('abcdefghijklmn.p')); // abcdefghijklmn
--- playground/dev/3)String/tempCodeRunnerFile.js
+++ playground/dev/3)String/tempCodeRunnerFile.js
@@ -1,5 +1,6 @@
-const toggleCase = str =>
-  str.replace(/([a-z]+)|([A-Z]+)/g, (_, lowerCase, upperCase) => {
-    console.log(_, lowerCase, upperCase);
-    return lowerCase ? lowerCase.toUpperCase() : upperCase.toLowerCase();
-  });
+// .replace(/[.]+/g, '.')
+// .replace(/(^[.])|([.]$)/g, '')
+// .replace(/^$/g, 'a')
+// .replace(/.+/g, match => (match.length >= 16 ? match.substring(0, 15) : match))
+// .replace(/[.]$/, '')
+// .replace(/.+/
